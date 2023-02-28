622192dad79a1f45790bc15f2bc894e14fbb0635
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-02-28 11:08:20 +0100

    fix dir

--- src/Compiler.php
+++ src/Compiler.php
@@ -3,39 +3,10 @@ namespace Tualo\Office\Dashboard;
 
 use Tualo\Office\Basic\TualoApplication;
 use Tualo\Office\ExtJSCompiler\ICompiler;
-use Tualo\Office\ExtJSCompiler\FileHelper;
+use Tualo\Office\ExtJSCompiler\CompilerHelper;
 
 class Compiler implements ICompiler {
-    
-
     public static function getFiles(){
-        $files = [];
-        $l = [];
-        FileHelper::listFiles(__DIR__."/js/modern",$l);
-        $files[] = [
-            'prio'=>'10003',
-            'toolkit'=>'modern',
-            'modul'=>'dashboard',
-            'files'=>$l
-        ];  
-
-        $l = [];
-        FileHelper::listFiles(__DIR__."/js/classic",$l);
-        $files[] = [
-            'prio'=>'10003',
-            'toolkit'=>'classic',
-            'modul'=>'dashboard',
-            'files'=>$l
-        ];  
-        
-        $l = [];
-        FileHelper::listFiles(__DIR__."/js/both",$l);
-        $files[] = [
-            'prio'=>'10003',
-            'toolkit'=>'',
-            'modul'=>'dashboard',
-            'files'=>$l
-        ];
-        return $files;
+        return CompilerHelper::getFiles(__DIR__,'dashboard',10003);
     }
 }
