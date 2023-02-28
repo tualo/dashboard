dea885dd6f367585cb455f5fe72e90702e8ea106
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-02-28 08:00:48 +0100

    list files

--- src/Compiler.php
+++ src/Compiler.php
@@ -6,27 +6,38 @@ use Tualo\Office\ExtJSCompiler\ICompiler;
 
 
 class Compiler implements ICompiler {
+    public static function listFiles($path){
+        $files=[];
+        if ($handle = opendir($path)) {
+            while (false !== ($file = readdir($handle))) {
+                if ( ($file!='.') && ($file!='..') ) $files[]=$file;
+            }
+            closedir($handle);
+        }
+        return $files;
+    }
+
     public static function getFiles(){
         $files = [];
         $files[] = [
             'prio'=>'10003',
             'toolkit'=>'modern',
             'modul'=>'dashboard',
-            'files'=>glob(__DIR__."/js/modern/*.js")
+            'files'=>self::listFiles(__DIR__."/js/modern/")
         ];  
 
         $files[] = [
             'prio'=>'10003',
             'toolkit'=>'classic',
             'modul'=>'dashboard',
-            'files'=>glob(__DIR__."/js/classic/*.js")
+            'files'=>self::listFiles(__DIR__."/js/classic/")
         ];  
         
         $files[] = [
             'prio'=>'10003',
             'toolkit'=>'',
             'modul'=>'dashboard',
-            'files'=>glob(__DIR__."/js/both/*.js")
+            'files'=>self::listFiles(__DIR__."/js/both/")
         ];
         return $files;
     }
