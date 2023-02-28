0e7cfdf62b845602cee007cef66cdb0debaf67ad
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-02-28 08:25:58 +0100

    use FileHelper

--- src/Compiler.php
+++ src/Compiler.php
@@ -3,35 +3,15 @@ namespace Tualo\Office\Dashboard;
 
 use Tualo\Office\Basic\TualoApplication;
 use Tualo\Office\ExtJSCompiler\ICompiler;
-
+use Tualo\Office\ExtJSCompiler\FileHelper;
 
 class Compiler implements ICompiler {
-    public static function listFiles($path,&$files,$replacesubpath=''){
-        if ($replacesubpath=='') $replacesubpath=$path.'/';
-        if (file_exists($path)){
-            if ($handle = opendir($path)) {
-                while (false !== ($file = readdir($handle))) {
-                    if ( ($file!='.') && ($file!='..') ){
-                        if (is_dir($path.'/'.$file)){
-                            self::listFiles($path.'/'.$file,$files,$replacesubpath);
-                        }else{
-                            $files[]=[
-                                'file'=>$path.'/'.$file,
-                                'subpath'=>dirname(str_replace($replacesubpath,'',$path.'/'.$file)),
-                                'prio'=>0
-                            ];
-                        }
-                    }
-                }
-                closedir($handle);
-            }
-        }
-    }
+    
 
     public static function getFiles(){
         $files = [];
         $l = [];
-        self::listFiles(__DIR__."/js/modern",$l);
+        FileHelper::listFiles(__DIR__."/js/modern",$l);
         $files[] = [
             'prio'=>'10003',
             'toolkit'=>'modern',
@@ -40,7 +20,7 @@ class Compiler implements ICompiler {
         ];  
 
         $l = [];
-        self::listFiles(__DIR__."/js/classic",$l);
+        FileHelper::listFiles(__DIR__."/js/classic",$l);
         $files[] = [
             'prio'=>'10003',
             'toolkit'=>'classic',
@@ -49,7 +29,7 @@ class Compiler implements ICompiler {
         ];  
         
         $l = [];
-        self::listFiles(__DIR__."/js/both",$l);
+        FileHelper::listFiles(__DIR__."/js/both",$l);
         $files[] = [
             'prio'=>'10003',
             'toolkit'=>'',
