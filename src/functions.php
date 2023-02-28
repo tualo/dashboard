661b098873207f6deda75d2741c1f357e86b2782
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-02-28 07:54:44 +0100

    include compiler

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,6 @@
 <?php
 
-if (class_exists("Tualo\Office\Dashboard\Middlewares\DashboardMiddleware")){ }
-if (class_exists("Tualo\Office\Dashboard\Routes\DashboardRoute")){ }
+//if (class_exists("Tualo\Office\Dashboard\Middlewares\DashboardMiddleware")){ }
+//if (class_exists("Tualo\Office\Dashboard\Routes\DashboardRoute")){ }
 
+require_once "Compiler.php";
