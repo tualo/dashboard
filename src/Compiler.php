b3fb8a4325941eba549bf2b654d7bfa0624b100a
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-02-28 08:21:21 +0100

    replace subpath

--- src/Compiler.php
+++ src/Compiler.php
@@ -6,15 +6,20 @@ use Tualo\Office\ExtJSCompiler\ICompiler;
 
 
 class Compiler implements ICompiler {
-    public static function listFiles($path,&$files){
+    public static function listFiles($path,&$files,$replacesubpath=''){
+        if ($replacesubpath=='') $replacesubpath=$path;
         if (file_exists($path)){
             if ($handle = opendir($path)) {
                 while (false !== ($file = readdir($handle))) {
                     if ( ($file!='.') && ($file!='..') ){
                         if (is_dir($path.'/'.$file)){
-                            self::listFiles($path.'/'.$file,$files);
+                            self::listFiles($path.'/'.$file,$files,$replacesubpath);
                         }else{
-                            $files[]=$path.'/'.$file;
+                            $files[]=[
+                                'file'=>$path.'/'.$file,
+                                'subpath'=>str_replace($replacesubpath,'',$path.'/'.$file),
+                                'prio'=>0
+                            ];
                         }
                     }
                 }
