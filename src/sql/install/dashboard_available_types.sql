eb9fe5fe944fec0fbd6ed9268b7578a4b83c1e32
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-18 16:32:57 +0200

    fix class name

--- src/sql/install/dashboard_available_types.sql
+++ src/sql/install/dashboard_available_types.sql
@@ -1,4 +1,6 @@
 DELIMITER ;
+insert ignore into ds_class (class_name) values ('Dashboard') ;
+
 CREATE TABLE IF NOT EXISTS `dashboard_available_types` (
   `xtype` varchar(255) NOT NULL,
   `classname` varchar(255) NOT NULL,
